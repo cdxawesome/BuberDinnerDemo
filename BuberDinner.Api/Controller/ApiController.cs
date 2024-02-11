5893d236288b9686f54fe0dc3137addd8ba0fff6
Dylan Chen <manomaster@163.com>
2024-02-11 13:00:40 +0800

    通用的MediatR管道

--- BuberDinner.Api/Controller/ApiController.cs
+++ BuberDinner.Api/Controller/ApiController.cs
@@ -10,21 +10,26 @@ namespace BuberDinner.Api.Controller
     {
         protected IActionResult Problem(List<Error> errors)
         {
+            if (errors.Count == 0)
+            {
+                return Problem();
+            }
             // 如果所有的错误都是Validation错误，返回ValidationProblem
             if (errors.All(err => err.Type == ErrorType.Validation))
             {
-                var modelStateDictionary = new ModelStateDictionary();
-
-                foreach (var error in errors)
-                {
-                    modelStateDictionary.AddModelError(error.Code, error.Description);
-                }
-                return ValidationProblem(modelStateDictionary);
+                return ValidationProblem(errors);
             }
 
             HttpContext.Items[HttpContextItemKeys.Errors] = errors;
             var firstError = errors.First();
 
+            return Problem(firstError);
+        }
+
+        /*这里将方法提取出来，命名和ControllerBase下的方法一样，但是方法签名不同。*/
+
+        private IActionResult Problem(Error firstError)
+        {
             var statusCode = firstError.Type switch
             {
                 ErrorType.Conflict => StatusCodes.Status409Conflict,
@@ -33,6 +38,17 @@ namespace BuberDinner.Api.Controller
                 _ => StatusCodes.Status500InternalServerError
             };
             return Problem(statusCode: statusCode, title: firstError.Description);
+        }
+
+        private IActionResult ValidationProblem(List<Error> errors)
+        {
+            var modelStateDictionary = new ModelStateDictionary();
+
+            foreach (var error in errors)
+            {
+                modelStateDictionary.AddModelError(error.Code, error.Description);
+            }
+            return ValidationProblem(modelStateDictionary);
         }
     }
 }
---
+++ BuberDinner.Application/Authentication/Queries/Login/LoginQueryValidator.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace BuberDinner.Application.Authentication.Queries.Login
+{
+    public class LoginQueryValidator : AbstractValidator<LoginQuery>
+    {
+        public LoginQueryValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("邮箱不能为空")
+                .EmailAddress().WithMessage("邮箱格式不正确");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("密码不能为空");
+        }
+    }
+}
