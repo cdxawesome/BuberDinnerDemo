0a76495c4e45b4cdef4e144b7921650f924ff1c4
Dylan Chen <manomaster@163.com>
2024-02-19 15:21:09 +0800

    AggregateRoot,Entity,ValueObject

---
+++ BuberDinner.Domain/Menu/Entities/MenuSection.cs
@@ -0,0 +1,11 @@
+using BuberDinner.Domain.Common.Models;
+using BuberDinner.Domain.Menu.ValueObjects;
+
+namespace BuberDinner.Domain.Menu.Entities;
+
+public sealed class MenuSection : Entity<MenuSectionId>
+{
+    private readonly List<MenuItem> _items = new();
+    public string Name { get; }
+    public string Description { get; }
+}
---
+++ BuberDinner.Domain/Menu/Menu.cs
@@ -0,0 +1,13 @@
+using BuberDinner.Domain.Common.Models;
+using BuberDinner.Domain.Menu.ValueObjects;
+
+namespace BuberDinner.Domain.Menu;
+
+public sealed class Menu:AggregateRoot<MenuId>
+{
+    private readonly List<MenuSection> _sections = new();
+
+    public string Name { get; }
+    public string Description { get; }
+    public string AverageRating { get; }
+}
